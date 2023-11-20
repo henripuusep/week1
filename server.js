16656fd4612b997fcf34e9898342ef416dfe68ed
Henri Puusep <henripuusep12@gmail.com>
2023-11-20 19:18:57 +0200

    week 1 code

---
+++ server.js
@@ -0,0 +1,11 @@
+//console.log("Hello world!");
+let firstname = "Harry";
+let lastName = "Potter";
+//console.log(firstname);
+
+//template literals
+
+//console.log(`Hello, ${firstname}!`);
+
+let greeting = `Hello, ${firstname} ${lastName}!`;
+console.log(greeting)
